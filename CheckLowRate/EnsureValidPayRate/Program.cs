ab61d2711aa525790c95574c5686de5477ab2a09
rbnsfr <rbnsfr@outlook.com>
2014-09-11 08:38:45 -0400

    Chapter 3 - Update

--- CheckLowRate/EnsureValidPayRate/Program.cs
+++ CheckLowRate/EnsureValidPayRate/Program.cs
@@ -9,24 +9,22 @@ namespace EnsureValidPayRate
     {
         static void Main(string[] args)
         {
-            Console.Write("What is your hourly pay rate? ");
-
-            if (Convert.ToDouble(Console.ReadLine()) < 5.65 || Convert.ToDouble(Console.ReadLine()) > 49.99)
-            {
-                Console.Write("That didn't look right. Type it again: ");
-
-                if (Convert.ToDouble(Console.ReadLine()) < 5.65 || Convert.ToDouble(Console.ReadLine()) > 49.99)
+            Console.Write("What is your hourly pay rate? $");
+            double payrate = Convert.ToDouble(Console.ReadLine());
+            if (payrate < 5.65 || payrate > 49.99) {
+                Console.Write("That didn't look right. Type it again: $");
+                payrate = Convert.ToDouble(Console.ReadLine());
+                if (payrate < 5.65 || payrate > 49.99)
                     Console.WriteLine("ERROR: Your hourly pay rate isn't valid.");
-                if (!(Convert.ToDouble(Console.ReadLine()) < 5.65 || Convert.ToDouble(Console.ReadLine()) > 49.99))
+                else
                 {
-                    Console.WriteLine("Pay rate: {0}", Convert.ToString(Convert.ToDouble(Console.ReadLine())));
-                    Console.WriteLine("Weekly rate: {0}", (Convert.ToString(Convert.ToDouble(Console.ReadLine()) * 40)));
+                    Console.WriteLine("\n- Pay rate: ${0}", payrate);
+                    Console.WriteLine("- Weekly rate: ${0}", Convert.ToString(Convert.ToDouble(payrate)*40));
                 }
             }
-            if (!(Convert.ToDouble(Console.ReadLine()) < 5.65 || Convert.ToDouble(Console.ReadLine()) > 49.99))
-            {
-                Console.WriteLine("Pay rate: {0}", Console.ReadLine());
-                Console.WriteLine("Weekly rate: {0}", (Convert.ToDouble(Console.ReadLine()) * 40).ToString());
+            else {
+                Console.WriteLine("\n- Pay rate: ${0}", Console.ReadLine());
+                Console.WriteLine("- Weekly rate: ${0}", Convert.ToString(Convert.ToDouble(payrate)*40));
             }
         }
     }
--- CheckLowRate/Furniture/Program.cs
+++ CheckLowRate/Furniture/Program.cs
@@ -9,7 +9,24 @@ namespace Furniture
     {
         static void Main(string[] args)
         {
-            // Finish this later
+            Console.WriteLine("╔══════════════════════╗");
+            Console.WriteLine("║    Furniture Shop    ║");
+            Console.WriteLine("╚══════════════════════╝");
+            Console.WriteLine("┌─────────────┬────────┐");
+            Console.WriteLine("│   [P]ine    │  $100  │");
+            Console.WriteLine("├─────────────┼────────┤");
+            Console.WriteLine("│    [O]ak    │  $225  │");
+            Console.WriteLine("├─────────────┼────────┤");
+            Console.WriteLine("│ [M]ahogany  │  $310  │");
+            Console.WriteLine("└─────────────┴────────┘");
+            Console.Write("\n Please choose a table to buy: ");
+
+            string choice = Console.ReadLine().ToLower();
+
+            if (choice == "p" || choice == "pine") Console.WriteLine("\n Thank you for choosing the least expensive one, you dirty cheapskate.");
+            else if (choice == "o" || choice == "oak") Console.WriteLine("\n Thank you for shopping with us. Now get out.");
+            else if (choice == "m" || choice == "mahogany") Console.WriteLine("\n Thank you for burning your money on worthless cosmetics.");
+            else Console.WriteLine("\n Since you were too incompetent to follow simple directions, we went ahead and\n charged you with the oak table. Have fun explaining this one to your family.");
         }
     }
 }
